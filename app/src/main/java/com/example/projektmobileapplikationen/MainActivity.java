f81bb655ca8740e0bfb36a9fffc3b0e49ae0c7fa
Florian292 <florianwolf100@outlook.com>
2019-10-18 11:58:57 +0200

    Test für Startseite

--- app/src/main/java/com/example/projektmobileapplikationen/MainActivity.java
+++ app/src/main/java/com/example/projektmobileapplikationen/MainActivity.java
@@ -3,11 +3,29 @@ package com.example.projektmobileapplikationen;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.*;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.info:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
