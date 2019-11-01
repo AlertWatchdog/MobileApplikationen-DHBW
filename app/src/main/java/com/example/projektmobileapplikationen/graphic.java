c4c6bf7e8e5c4788c72c5485ed4cb92023c2c926
Benedikt Strozynski <strozynskib@yahoo.de>
2019-11-02 00:19:44 +0100

    Übernahme der Daten aus der DB, zum Erzeugen der Graphic. Läuft jetzt dynamisch mit dem ArrayList Objekt von DBHandler.

--- app/src/main/java/com/example/projektmobileapplikationen/graphic.java
+++ app/src/main/java/com/example/projektmobileapplikationen/graphic.java
@@ -1,6 +1,7 @@
 package com.example.projektmobileapplikationen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.graphics.Paint;
@@ -8,7 +9,6 @@ import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.content.Context;
 import android.graphics.Color;
-
 
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
@@ -18,65 +18,81 @@ import org.achartengine.model.XYMultipleSeriesDataset;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class graphic extends AppCompatActivity {
 
+    private DBHandler db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+//////////////////////////////////////////////////////////////////////////////////////////
+        db = new DBHandler(this);
 
-        int xlabelmax = 10;
-        int ylabelmax = 10;
+        List <Reise> resultate = db.getAllTrips();
 
-        int[] y = {1, 33, 53, 45};
+
+        //Höhe des Graphen setzen
+        int ylabelmax = 0;
+        for (int i = 0; i< resultate.size();i++){
+            if(Integer.parseInt(resultate.get(i).getAuszahlung())>ylabelmax){
+                ylabelmax=Integer.parseInt(resultate.get(i).getAuszahlung());
+                }
+        }
+        ylabelmax = (int)(ylabelmax*1.2);
+        //Breite des Graphen setzen
+        int xlabelmax = resultate.size()+1;
+//////////////////////////////////////////////////////////////////////////////////////////
+
 
         CategorySeries series = new CategorySeries("Demo Bar Chart Graph...");
-        for(int i =0; i<y.length;i++){
-            series.add("Bar " + (i+1), y[i]);
+        for (int i = 0; i < resultate.size(); i++) {
+            series.add(Double.parseDouble(resultate.get(i).getAuszahlung()));
         }
 
         XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
         dataset.addSeries(series.toXYSeries());
 
-        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+        //hier Balken Änderungen vornehmen.
         XYSeriesRenderer renderer = new XYSeriesRenderer();
+        renderer.setDisplayChartValues(true);
+        renderer.setChartValuesSpacing((float) 0.5);
+        renderer.setColor(Color.BLACK);
+        renderer.setChartValuesTextSize(50);
+
+        //ab hier den Graphen Änderungen vornehmen
+        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
         mRenderer.addSeriesRenderer(renderer);
 
-        setChartSettings(mRenderer, "Reisekosten", "Reise: ", "Betrag: ", 0, (double)xlabelmax, 0, (double)ylabelmax, Color.LTGRAY, Color.LTGRAY);
-
+        mRenderer.setChartTitleTextSize(100);
+        mRenderer.setShowLegend(false);
+        mRenderer.setChartTitle("Reisekosten");
+        mRenderer.setXTitle("Reise");
+        mRenderer.setAxisTitleTextSize(50);
+        mRenderer.setYTitle("Betrag in €");
+        mRenderer.setXAxisMin(0);
+        mRenderer.setXAxisMax((double) xlabelmax);
+        mRenderer.setYAxisMin(0);
+        mRenderer.setYAxisMax((double) ylabelmax);
+        mRenderer.setAxesColor(Color.BLACK);
+        mRenderer.setYAxisColor(Color.BLACK);
+        mRenderer.setLabelsColor(Color.BLUE);
         mRenderer.setXLabels(5);
         mRenderer.setYLabels(10);
-        mRenderer.setLabelsTextSize(50);
+        mRenderer.setLabelsTextSize(30);
         mRenderer.setXLabelsAlign(Paint.Align.RIGHT);
         mRenderer.setYLabelsAlign(Paint.Align.RIGHT);
         mRenderer.setMarginsColor(Color.WHITE);
         mRenderer.setPanEnabled(false);
         //mRenderer.setZoomButtonsVisible(true);
-        mRenderer.setPanLimits(new double[] { -10, 20, -10, 40 });
-        mRenderer.setZoomLimits(new double[] { -10, 20, -10, 40 });
+        //mRenderer.setPanLimits(new double[]{-10, 20, -10, 40});
+        //mRenderer.setZoomLimits(new double[]{-10, 20, -10, 40});
 
 
         GraphicalView chartview = ChartFactory.getBarChartView(getApplicationContext(), dataset, mRenderer, BarChart.Type.DEFAULT);
         setContentView(chartview);
 
     }
-    private void setChartSettings(XYMultipleSeriesRenderer mRenderer,
-                                  String title, String xTitle, String yTitle, double xMin,
-                                  double xMax, double yMin, double yMax, int axesColor,
-                                  int labelsColor) {
-        mRenderer.setChartTitle(title);
-        mRenderer.setXTitle(xTitle);
-        mRenderer.setYTitle(yTitle);
-        mRenderer.setXAxisMin(xMin);
-        mRenderer.setXAxisMax(xMax);
-        mRenderer.setYAxisMin(yMin);
-        mRenderer.setYAxisMax(yMax);
-        mRenderer.setAxesColor(axesColor);
-        mRenderer.setLabelsColor(labelsColor);
-    }
-
-
-
-
-
 }
